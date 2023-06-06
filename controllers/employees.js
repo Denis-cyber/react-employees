36472aac9404f8ee22c37794ee87b33546e3b200
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-06-06 23:59:41 +0300

    add requests for employees

--- controllers/employees.js
+++ controllers/employees.js
@@ -11,7 +11,7 @@ const all = async (req, res) => {
 
     res.status(200).json(employees);
   } catch {
-    res.status(400).json({ message: "Не удалось получить сотрудников" });
+    res.status(500).json({ message: "Не удалось получить сотрудников" });
   }
 };
 
@@ -22,30 +22,94 @@ const all = async (req, res) => {
  */
 const add = async (req, res) => {
   try {
-    const { firstName, lastName, address, age } = req.body;
+    const data = req.body;
 
-    if (!data.firstName || !data.lastName) {
-      return res.status(500).json({ message: "Все поля обязательные" });
+    if (!data.firstName || !data.lastName || !data.address || !data.age) {
+      return res.status(400).json({ message: "Все поля обязательные" });
     }
 
-    const employee = await prisma.user.update({
-      where: {
-        id: req.user.id,
-      },
+    const employee = await prisma.employee.create({
       data: {
-        createdEmployee: {
-          create: data,
-        },
+        ...data,
+        userId: req.user.id,
       },
     });
 
-    return res.status(201).json(employee);
+    res.status(201).json(employee);
   } catch {
     res.status(500).json({ message: "Что-то пошло не так" });
+  }
+};
+
+/**
+ * @route POST /api/employees/remove/:id
+ * @desс remove employee
+ * @access Private
+ */
+const remove = async (req, res) => {
+  try {
+    const { id } = req.body;
+
+    await prisma.employee.delete({
+      where: {
+        id,
+      },
+    });
+
+    res.status(204).json("OK");
+  } catch {
+    res.status(500).json({ message: "Не удалось удалить сотрудника" });
+  }
+};
+
+/**
+ * @route PUT /api/employees/edit/:id
+ * @desс edit info employee
+ * @access Private
+ */
+const edit = async (req, res) => {
+  try {
+    const data = req.body;
+    const id = data.id;
+
+    await prisma.employee.update({
+      where: {
+        id,
+      },
+      data,
+    });
+
+    res.status(204).json("OK");
+  } catch {
+    res.status(500).json({ message: "Не удалось редактировать информацию о сотруднике" });
+  }
+};
+
+/**
+ * @route GET /api/employees/:id
+ * @desс get employee
+ * @access Private
+ */
+const employee = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const employee = await prisma.employee.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    res.status(200).json(employee);
+  } catch {
+    res.status(500).json({ message: "Не удалось получить сотрудника" });
   }
 };
 
 module.exports = {
   all,
   add,
+  remove,
+  edit,
+  employee,
 };
