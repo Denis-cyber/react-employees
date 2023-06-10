abf12e61da48f15bfa5ab759efa00c01ffd35bbc
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-06-10 12:52:01 +0300

    page with employees

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,12 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Layout, Space, Typography } from "antd";
 import { LoginOutlined, TeamOutlined, UserOutlined } from "@ant-design/icons";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { logout, selectUser } from "../../features/auth/authSlice";
 import { Paths } from "../../paths";
 import { CustomButton } from "../customButton";
 
 import styles from "./index.module.css";
 
 export const Header = () => {
+  const user = useAppSelector(selectUser);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const onLogoutClick = () => {
+    dispatch(logout());
+    localStorage.removeItem("token");
+    navigate(Paths.login);
+  };
+
   return (
     <Layout.Header className={styles.header}>
       <Space>
@@ -18,18 +30,24 @@ export const Header = () => {
         </Link>
       </Space>
 
-      <Space>
-        <Link to={Paths.register}>
-          <CustomButton icon={<UserOutlined />} type='ghost'>
-            Зарегистрироваться
-          </CustomButton>
-        </Link>
-        <Link to={Paths.login}>
-          <CustomButton icon={<LoginOutlined />} type='ghost'>
-            Войти
-          </CustomButton>
-        </Link>
-      </Space>
+      {user ? (
+        <CustomButton type='ghost' icon={<LoginOutlined />} onClick={onLogoutClick}>
+          Выйти
+        </CustomButton>
+      ) : (
+        <Space>
+          <Link to={Paths.register}>
+            <CustomButton icon={<UserOutlined />} type='ghost'>
+              Зарегистрироваться
+            </CustomButton>
+          </Link>
+          <Link to={Paths.login}>
+            <CustomButton icon={<LoginOutlined />} type='ghost'>
+              Войти
+            </CustomButton>
+          </Link>
+        </Space>
+      )}
     </Layout.Header>
   );
 };
--- client/src/pages/employees/index.tsx
+++ client/src/pages/employees/index.tsx
@@ -1,8 +1,60 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Employee } from "@prisma/client";
+import { Table } from "antd";
+import { PlusCircleOutlined } from "@ant-design/icons";
+import type { ColumnsType } from "antd/es/table";
+import { CustomButton } from "../../components/customButton";
+import { Layout } from "../../components/layout";
+import { useGetAllEmployeesQuery } from "../../app/services/employees";
+import { Paths } from "../../paths";
 import { useAppSelector } from "../../app/hooks";
+import { selectUser } from "../../features/auth/authSlice";
+
+const columns: ColumnsType<Employee> = [
+  {
+    title: "Имя",
+    dataIndex: "firstName",
+    key: "firstName",
+  },
+  {
+    title: "Возраст",
+    dataIndex: "age",
+    key: "age",
+  },
+  {
+    title: "Адрес",
+    dataIndex: "address",
+    key: "address",
+  },
+];
 
 export const Employees = () => {
-  const store = useAppSelector((s) => s);
+  const navigate = useNavigate();
+  const user = useAppSelector(selectUser);
+  const { data, isLoading } = useGetAllEmployeesQuery();
 
-  console.log(store);
-  return <h1>Employees</h1>;
+  const goToAddUser = () => navigate(Paths.employeeAdd);
+
+  useEffect(() => {
+    if (!user) {
+      navigate(Paths.login);
+    }
+  }, [navigate, user]);
+
+  return (
+    <Layout>
+      <CustomButton type='primary' onClick={goToAddUser} icon={<PlusCircleOutlined />}>
+        Добавить
+      </CustomButton>
+      <Table
+        loading={isLoading}
+        dataSource={data}
+        pagination={false}
+        columns={columns}
+        rowKey={(record) => record.id}
+        onRow={(record) => ({ onClick: () => navigate(`${Paths.employee}/${record.id}`) })}
+      />
+    </Layout>
+  );
 };
